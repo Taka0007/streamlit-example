cf29fb3cff669f3b66f2cc8ed0b640a2e20b4d78
Taka <103984069+Taka0007@users.noreply.github.com>
2023-10-02 02:39:17 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,24 +15,76 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+import streamlit as st
+import json
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# カラオケ曲のダミーデータ（JSON形式）
+karaoke_data = {
+    "songs": [
+        {
+            "title": "曲名1",
+            "artist": "アーティスト1",
+            "genre": "ジャンル1",
+            "rating": 4.5
+        },
+        {
+            "title": "曲名2",
+            "artist": "アーティスト2",
+            "genre": "ジャンル2",
+            "rating": 4.0
+        },
+        {
+            "title": "曲名3",
+            "artist": "アーティスト3",
+            "genre": "ジャンル1",
+            "rating": 4.2
+        },
+        {
+            "title": "曲名4",
+            "artist": "アーティスト4",
+            "genre": "ジャンル2",
+            "rating": 3.8
+        }
+    ]
+}
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Streamlitアプリの設定
+st.title("カラオケ曲の推薦アプリ")
 
-    points_per_turn = total_points / num_turns
+# ダミーデータを表示
+st.write("カラオケ曲一覧:")
+for song in karaoke_data["songs"]:
+    st.write(f"曲名: {song['title']}")
+    st.write(f"アーティスト: {song['artist']}")
+    st.write(f"ジャンル: {song['genre']}")
+    st.write(f"評価: {song['rating']}")
+    st.write("---")
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+# 以下は追加の機能を組み込む部分です
+# 1. フィルタリング機能
+genre_filter = st.selectbox("ジャンルを選択して曲をフィルタリング", ["ジャンル1", "ジャンル2"])
+filtered_songs = [song for song in karaoke_data["songs"] if song["genre"] == genre_filter]
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if filtered_songs:
+    st.write(f"{genre_filter} の曲一覧:")
+    for song in filtered_songs:
+        st.write(f"曲名: {song['title']}")
+        st.write(f"アーティスト: {song['artist']}")
+        st.write(f"評価: {song['rating']}")
+        st.write("---")
+else:
+    st.write("該当する曲がありません。")
+
+# 2. ソート機能
+sort_option = st.radio("曲を評価でソート", ["昇順", "降順"])
+if sort_option == "昇順":
+    sorted_songs = sorted(karaoke_data["songs"], key=lambda x: x["rating"])
+else:
+    sorted_songs = sorted(karaoke_data["songs"], key=lambda x: x["rating"], reverse=True)
+
+st.write("ソートされた曲一覧:")
+for song in sorted_songs:
+    st.write(f"曲名: {song['title']}")
+    st.write(f"アーティスト: {song['artist']}")
+    st.write(f"評価: {song['rating']}")
+    st.write("---")
